3f29ec6b29d52e8536a6af1db78961a11af65b1f
Tamim <tamim.hossen@transcombd.com>
2025-07-16 10:46:10 +0600

    aa

--- cpanel_rnd.sql
+++ cpanel_rnd.sql
@@ -85,14 +85,17 @@ CREATE TABLE `u_role_has_tasks` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `u_role_has_tasks` */
 
 insert  into `u_role_has_tasks`(`id`,`cid`,`pid`,`role_id`,`role_name`,`task_id`,`task_name`,`group_id`,`group_name`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','cpanel','1','super_admin','1','login','1','access_management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(2,'TCL','cpanel','1','super_admin','2','business_units_create','1','access_management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3,'TCL','cpanel','1','super_admin','3','role_management','1','access_management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+(22,'TCL',NULL,'1','super_admin','2','business_units_create',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
+(23,'TCL',NULL,'1','super_admin','3','role_management',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
+(24,'TCL',NULL,'1','super_admin','4','user_create',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
+(25,'TCL',NULL,'1','super_admin','6','user_delete',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
+(26,'TCL',NULL,'1','super_admin','5','user_view',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
+(27,'TCL',NULL,'3','normal_user','5','user_view',NULL,NULL,1,'',0,'','2025-07-16 16:24:46',NULL,'2025-07-16 16:24:46',NULL);
 
 /*Table structure for table `u_roles` */
 
@@ -111,12 +114,13 @@ CREATE TABLE `u_roles` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `u_roles` */
 
 insert  into `u_roles`(`id`,`cid`,`pid`,`role_name`,`role_description`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','cpanel','super_admin','Super Admin',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+(1,'TCL','cpanel','super_admin','Super Admin',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(3,'TCL','surjo','normal_user','normal user',1,'',0,'','2025-07-15 23:50:48',NULL,'2025-07-15 23:54:15',NULL);
 
 /*Table structure for table `u_task_group` */
 
@@ -163,20 +167,23 @@ CREATE TABLE `u_tasks` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `u_tasks` */
 
 insert  into `u_tasks`(`id`,`cid`,`pid`,`task_name`,`task_description`,`group_id`,`group_name`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','cpanel','login','login system','1',NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(2,'TCL','cpanel','business_units_create','business_units_create','1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3,'TCL','cpanel','role_management','role_management','1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+(2,'TCL','cpanel','business_units_create','business_units_create','1','access_management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(3,'TCL','cpanel','role_management','role_management','1','access_management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(4,'TCL','cpanel','user_create','create user','2','user_management',1,'',0,'','2025-07-16 16:03:23',NULL,'2025-07-16 16:03:23',NULL),
+(5,'TCL','cpanel','user_view','View user','2','user_management',1,'',0,'','2025-07-16 16:03:36',NULL,'2025-07-16 16:03:36',NULL),
+(6,'TCL','cpanel','user_delete','Delete User','2','user_management',1,'',0,'','2025-07-16 16:17:26',NULL,'2025-07-16 16:17:26',NULL);
 
 /*Table structure for table `users` */
 
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `cid` varchar(20) DEFAULT NULL,
+  `pid` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `user_id` varchar(20) DEFAULT NULL,
   `first_name` varchar(100) DEFAULT NULL,
   `last_name` varchar(100) DEFAULT NULL,
@@ -207,12 +214,13 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`cid`,`user_id`,`first_name`,`last_name`,`full_name`,`email`,`mobile`,`password`,`gender`,`location`,`user_type`,`image_path`,`role_id`,`user_role`,`status`,`device_id`,`sync_code`,`app_version`,`otp_token`,`token_expire_time`,`otp_status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','1001','super','admin','super admin','admin@gmail.com','+8801710000001','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Male','Gulshan 2','admin',NULL,'1','super_admin',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert  into `users`(`id`,`cid`,`pid`,`user_id`,`first_name`,`last_name`,`full_name`,`email`,`mobile`,`password`,`gender`,`location`,`user_type`,`image_path`,`role_id`,`user_role`,`status`,`device_id`,`sync_code`,`app_version`,`otp_token`,`token_expire_time`,`otp_status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
+(1,'TCL','cpanel','1001','super','admin','super admin','admin@gmail.com','+8801710000001','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Male','Gulshan 2','admin',NULL,'1','super_admin',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(2,'PALO','surjo','1002','normal','user','normal user','normal@transcombd.com','12345678','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Male','asdfgn','permanent','default_profile.png','3','normal_user',1,'-',0,NULL,0,NULL,0,'',0,'','2025-07-16 16:24:26',NULL,'2025-07-16 16:44:21',NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
